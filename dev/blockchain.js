25e0f04b378b3ca7e731a7db0ba2995f2a239653
martinwachira <martinnwachira@gmail.com>
2024-02-12 21:56:12 +0300

    init block getter and transactions setter

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,6 @@
 function Blockchain() {
   this.chain = [];
-  this.newTransactions = [];
+  this.pendingTransactions = [];
 }
 
 Blockchain.prototype.createNewBlock = function (
@@ -11,16 +11,36 @@ Blockchain.prototype.createNewBlock = function (
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
-    transactions: this.newTransactions,
+    transactions: this.pendingTransactions,
     nonce: nonce,
     hash: hash,
     previousBlockHash: previousBlockHash,
   };
 
-  this.newTransactions = [];
+  this.pendingTransactions = [];
   this.chain.push(newBlock);
 
   return newBlock;
+};
+
+Blockchain.prototype.getLastBlock = function () {
+  return this.chain[this.chain.length - 1];
+};
+
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient,
+  };
+
+  this.pendingTransactions.push(newTransaction);
+
+  return this.getLastBlock()["index"] + 1;
 };
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -6,4 +6,6 @@ bitcoin.createNewBlock(2436, "N0QE73254HWSF", "DJ0DSF23WE330");
 bitcoin.createNewBlock(340, "783463VJHDSF3", "DJ0DSF23WE330");
 bitcoin.createNewBlock(1110, "PLC3240SFSDD", "99JDSF82R323D");
 
+bitcoin.createNewTransaction(20, "test-sender", "test-recipient");
+
 console.log("bitcoin = ", bitcoin);
