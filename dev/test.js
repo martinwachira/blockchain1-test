13899a204bd1088dac9cd9eba9a6b9738c1febe9
martinwachira <martinnwachira@gmail.com>
2024-02-15 15:36:43 +0300

    test new transactions

--- dev/test.js
+++ dev/test.js
@@ -2,10 +2,16 @@ const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2436, "N0QE73254HWSF", "DJ0DSF23WE330");
 bitcoin.createNewBlock(340, "783463VJHDSF3", "DJ0DSF23WE330");
-bitcoin.createNewBlock(1110, "PLC3240SFSDD", "99JDSF82R323D");
 
 bitcoin.createNewTransaction(20, "test-sender", "test-recipient");
 
-console.log("bitcoin = ", bitcoin);
+bitcoin.createNewBlock(1110, "PLC3240SFSDD", "99JDSF82R323D");
+
+bitcoin.createNewTransaction(65, "test-sender1", "test-recipient1");
+bitcoin.createNewTransaction(76, "test-sender1", "test-recipient1");
+bitcoin.createNewTransaction(100, "test-sender1", "test-recipient1");
+
+bitcoin.createNewBlock(8546, "DSJHFWR23WSDF", "3894HFNEJDSDF3");
+
+console.log(bitcoin.chain[2]);
