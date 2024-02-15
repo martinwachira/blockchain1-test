fb07237eb30dcf7e547af96ed26fb1874f061454
martinwachira <martinnwachira@gmail.com>
2024-02-15 17:34:08 +0300

    init proof of work method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -56,4 +56,18 @@ Blockchain.prototype.hashBlock = function (
   return hash;
 };
 
+Blockchain.prototype.proofOfWork = function (
+  previousBlockHash,
+  currentBlockData
+) {
+  let nonce = 0;
+  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  while (hash.substring(0, 4) !== "0000") {
+    nonce += 1;
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  }
+
+  return nonce;
+};
+
 module.exports = Blockchain;
