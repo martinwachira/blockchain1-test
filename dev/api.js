ddc0f239d4f38e3c6e6a14f7cd09b94a98672d44
martinwachira <martinnwachira@gmail.com>
2024-02-19 17:09:46 +0300

    test api call

--- dev/api.js
+++ dev/api.js
@@ -1,9 +1,16 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ encoded: false }));
 
 app.get("/blockchain", function (req, res) {});
 
-app.post("/transaction", function (req, res) {});
+app.post("/transaction", function (req, res) {
+  console.log(req.body);
+  res.send(`amount of transaction is ${req.body.amount} bitcoins`);
+});
 
 app.get("/mine", function (req, res) {});
 
