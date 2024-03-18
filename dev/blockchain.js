6e2a66682279de1abc8a0a8bd487f3e2d60a966f
martinwachira <martinnwachira@gmail.com>
2024-03-18 17:42:10 +0300

    get nodes from the pkg file

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,8 +1,12 @@
 const sha256 = require("sha256");
+const currentNodeUrl = process.argv[3];
 
 function Blockchain() {
   this.chain = [];
   this.pendingTransactions = [];
+
+  this.currentNodeUrl = currentNodeUrl;
+  this.networkNodes = [];
 
   // genesis block
   this.createNewBlock(1, "0", "0");
